c1a44afc2b9ecf222ee3d81d12e485489c77cfb2
seref <serefacet@gmail.com>
2014-11-05 17:10:47 +0200

    firstcommit

--- src/main/java/com/university/mongodb/practise/App.java
+++ src/main/java/com/university/mongodb/practise/App.java
@@ -17,7 +17,7 @@ public class App
     public static void main( String[] args ) throws UnknownHostException {
         MongoClient mongoClient  = new MongoClient(new ServerAddress("localhost",27017));
         
-        DB database = mongoClient.getDB("course");
+        DB  database = mongoClient.getDB("course"); 
         DBCollection collection = database.getCollection("hello");
         DBObject document = collection.findOne();
         System.out.println(document);
